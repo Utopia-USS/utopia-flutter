e47ea396034fd0fe4020084d4ebdd0f4f343bd4f
Jakub Walusiak <jakubwalusiak@gmail.com>
2021-09-01 00:17:59 +0200

    fix(utils): do not handle Isolate errors in Web

--- packages/utils/lib/reporter/run_app_with_reporter.dart
+++ packages/utils/lib/reporter/run_app_with_reporter.dart
@@ -1,16 +1,23 @@
 import 'dart:async';
 import 'dart:isolate';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:utopia_utils/reporter/reporter.dart';
 
 void runAppWithReporter(Reporter reporter, Widget app) {
+  // handle flutter framework errors
   FlutterError.onError = reporter.flutterError;
-  Isolate.current.addErrorListener(RawReceivePort((pair) async {
-    final List<dynamic> errorAndStacktrace = pair;
-    reporter.error('Uncaught error', errorAndStacktrace.first, errorAndStacktrace.last);
-  }).sendPort);
 
+  // handle uncaught dart errors (not supported on Web)
+  if(!kIsWeb) {
+    Isolate.current.addErrorListener(RawReceivePort((pair) async {
+      final List<dynamic> errorAndStacktrace = pair;
+      reporter.error('Uncaught error', errorAndStacktrace.first, errorAndStacktrace.last);
+    }).sendPort);
+  }
+
+  // run app and handle uncaught failed futures
   runZonedGuarded(
     () => runApp(app),
     (e, s) => reporter.error('Uncaught error', e, s),
