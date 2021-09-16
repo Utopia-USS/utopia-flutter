5d1b010b93e4fc20f1e266a4d0f0dcbe12c4ef4a
Jakub Walusiak <jakubwalusiak@gmail.com>
2021-09-16 11:58:22 +0200

    feat(utopia_utils): add sanitizedMessage param to Reporter methods BREAKING CHANGE: converted error and stack parameters to named parameters

--- packages/utils/lib/reporter/reporter.dart
+++ packages/utils/lib/reporter/reporter.dart
@@ -3,13 +3,14 @@ import 'package:flutter/cupertino.dart';
 abstract class Reporter {
   const Reporter();
 
-  void flutterError(FlutterErrorDetails details) => error(details.toString(), details.exception, details.stack);
+  void flutterError(FlutterErrorDetails details) =>
+      error(details.toString(), e: details.exception, s: details.stack);
 
-  void error(String message, [Object? error, StackTrace? stackTrace]) {}
+  void error(String message, {Object? e, StackTrace? s, String? sanitizedMessage}) {}
 
-  void warning(String message, [Object? error, StackTrace? stackTrace]) {}
+  void warning(String message, {Object? e, StackTrace? s, String? sanitizedMessage}) {}
 
-  void info(String message, [Object? error, StackTrace? stackTrace]) {}
+  void info(String message, {Object? e, StackTrace? s, String? sanitizedMessage}) {}
 
   static Reporter combine(List<Reporter> reporters) => _CombinedReporter(reporters);
 }
@@ -20,14 +21,23 @@ class _CombinedReporter extends Reporter {
   const _CombinedReporter(this._reporters);
 
   @override
-  void error(String message, [Object? error, StackTrace? stackTrace]) =>
-      _reporters.forEach((reporter) => reporter.error(message, error, stackTrace));
+  void error(String message, {Object? e, StackTrace? s, String? sanitizedMessage}) {
+    _reporters.forEach(
+      (reporter) => reporter.error(message, e: e, s: s, sanitizedMessage: sanitizedMessage),
+    );
+  }
 
   @override
-  void warning(String message, [Object? error, StackTrace? stackTrace]) =>
-      _reporters.forEach((reporter) => reporter.warning(message, error, stackTrace));
+  void warning(String message, {Object? e, StackTrace? s, String? sanitizedMessage}) {
+    _reporters.forEach(
+      (reporter) => reporter.warning(message, e: e, s: s, sanitizedMessage: sanitizedMessage),
+    );
+  }
 
   @override
-  void info(String message, [Object? error, StackTrace? stackTrace]) =>
-      _reporters.forEach((reporter) => reporter.info(message, error, stackTrace));
+  void info(String message, {Object? e, StackTrace? s, String? sanitizedMessage}) {
+    _reporters.forEach(
+      (reporter) => reporter.info(message, e: e, s: s, sanitizedMessage: sanitizedMessage),
+    );
+  }
 }
--- packages/utils/lib/reporter/run_app_with_reporter.dart
+++ packages/utils/lib/reporter/run_app_with_reporter.dart
@@ -13,13 +13,13 @@ void runAppWithReporter(Reporter reporter, Widget app) {
   if(!kIsWeb) {
     Isolate.current.addErrorListener(RawReceivePort((pair) async {
       final List<dynamic> errorAndStacktrace = pair;
-      reporter.error('Uncaught error', errorAndStacktrace.first, errorAndStacktrace.last);
+      reporter.error('Uncaught error', e: errorAndStacktrace.first, s: errorAndStacktrace.last);
     }).sendPort);
   }
 
   // run app and handle uncaught failed futures
   runZonedGuarded(
     () => runApp(app),
-    (e, s) => reporter.error('Uncaught error', e, s),
+    (e, s) => reporter.error('Uncaught error', e: e, s: s),
   );
 }
