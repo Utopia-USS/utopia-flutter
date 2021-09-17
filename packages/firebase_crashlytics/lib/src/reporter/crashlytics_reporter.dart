a9584ce8966a767c71229bd7199dba82e99bb705
Jakub Walusiak <jakubwalusiak@gmail.com>
2021-09-17 14:14:54 +0200

    feat(utopia_firebase_crashlytics)!: Fix directory structure

--- packages/firebase_crashlytics/lib/src/reporter/crashlytics_reporter.dart
+++ packages/firebase_crashlytics/lib/src/reporter/crashlytics_reporter.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:utopia_utils/reporter/reporter.dart';
+import 'package:utopia_utils/utopia_utils.dart';
 
 class CrashlyticsReporter extends Reporter {
   const CrashlyticsReporter();
@@ -20,12 +20,10 @@ class CrashlyticsReporter extends Reporter {
   }
 
   @override
-  void warning(String message, {Object? e, StackTrace? s, String? sanitizedMessage}) =>
-      _log(message, sanitizedMessage);
+  void warning(String message, {Object? e, StackTrace? s, String? sanitizedMessage}) => _log(message, sanitizedMessage);
 
   @override
-  void info(String message, {Object? e, StackTrace? s, String? sanitizedMessage}) =>
-      _log(message, sanitizedMessage);
+  void info(String message, {Object? e, StackTrace? s, String? sanitizedMessage}) => _log(message, sanitizedMessage);
 
   void _log(String message, String? sanitizedMessage) {
     if (_isFirebaseInitialized) {
--- packages/firebase_crashlytics/lib/utopia_firebase_crashlytics.dart
+++ packages/firebase_crashlytics/lib/utopia_firebase_crashlytics.dart
@@ -3,6 +3,8 @@ import 'dart:async';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 
+export 'src/reporter/crashlytics_reporter.dart';
+
 class UtopiaFirebaseCrashlytics {
   static Future<void> setup() async {
     if (kDebugMode) {
