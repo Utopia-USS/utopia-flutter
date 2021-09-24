c407c2892f6e1439ab263176a957e59901447694
Jakub Walusiak <jakubwalusiak@gmail.com>
2021-09-24 11:48:59 +0200

    feat(utopia_connectivity): initial commit

---
+++ packages/connectivity/lib/src/state/connectivity_state.dart
@@ -0,0 +1,36 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:utopia_hooks/utopia_hooks.dart';
+
+class ConnectivityState {
+  final ConnectivityResult? result;
+  final Future<ConnectivityResult> Function() awaitInitialized;
+
+  const ConnectivityState({required this.result, required this.awaitInitialized});
+
+  bool get isInitialized => result != null;
+
+  bool get isConnected => result != ConnectivityResult.none;
+}
+
+class ConnectivityStateProvider extends HookStateProviderWidget<ConnectivityState> {
+  @override
+  ConnectivityState use() {
+    final state = useAutoComputedState<ConnectivityResult>(
+      compute: () async => await Connectivity().checkConnectivity(),
+      keys: [],
+    );
+
+    useStreamSubscription<ConnectivityResult>(
+      useMemoized(() => Connectivity().onConnectivityChanged),
+      (result) => state.updateValue(result),
+    );
+
+    return ConnectivityState(
+      result: state.valueOrNull,
+      awaitInitialized: () async {
+        if (state.valueOrNull != null) return state.valueOrNull!;
+        return await state.tryRefresh();
+      },
+    );
+  }
+}
---
+++ packages/connectivity/lib/utopia_connectivity.dart
@@ -0,0 +1,5 @@
+export 'src/state/connectivity_state.dart';
+
+class UtopiaConnectivity {
+  const UtopiaConnectivity._();
+}
