64afbd39be4460f3a3a081bf3af9bc5c0b6ae871
Jakub Walusiak <jakubwalusiak@gmail.com>
2021-10-19 21:14:08 +0200

    fix(utopia_arch): move LoggerReporter to utopia_utils

--- packages/arch/lib/utopia_arch.dart
+++ packages/arch/lib/utopia_arch.dart
@@ -4,10 +4,13 @@ export 'src/navigation/nested_navigator.dart';
 export 'src/navigation/route_config.dart';
 export 'src/navigation/scoped_navigator_state.dart';
 export 'src/navigation/use_scoped_navigator.dart';
-export 'src/reporter/logger_reporter.dart';
 export 'src/service/preferences/preferences_service.dart';
 export 'src/validation/validator.dart';
 export 'src/validation/validators.dart';
+
+// LoggerReporter was previously in utopia_arch, exported here for backwards compatibility reasons
+// TODO remove in next breaking release
+export 'package:utopia_utils/utopia_utils.dart' show LoggerReporter;
 
 class UtopiaArch {
   const UtopiaArch._();
