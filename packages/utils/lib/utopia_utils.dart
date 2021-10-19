1a96b2f524ce7d12e526a4d7c27b8d38f3bd720c
Jakub Walusiak <jakubwalusiak@gmail.com>
2021-10-19 21:13:38 +0200

    feat(utopia_utils): move LoggerReporter from utopia_arch

--- packages/utils/lib/utopia_utils.dart
+++ packages/utils/lib/utopia_utils.dart
@@ -1,5 +1,6 @@
 export 'src/comparator/compare_by.dart';
 export 'src/function/bool_functions.dart';
+export 'src/reporter/logger_reporter.dart';
 export 'src/reporter/reporter.dart';
 export 'src/reporter/run_app_with_reporter.dart';
 export 'utopia_utils_extensions.dart';
