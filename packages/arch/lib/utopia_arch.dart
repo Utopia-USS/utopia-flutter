d5bcfd7e83c6d61692976d4ddf678a88bb3c1657
Jakub Walusiak <jakubwalusiak@gmail.com>
2021-11-04 10:58:19 +0100

    fix(utopia_arch)!: remove LoggerReporter from utopia_arch

--- packages/arch/lib/utopia_arch.dart
+++ packages/arch/lib/utopia_arch.dart
@@ -8,10 +8,6 @@ export 'src/service/preferences/preferences_service.dart';
 export 'src/validation/validator.dart';
 export 'src/validation/validators.dart';
 
-// LoggerReporter was previously in utopia_arch, exported here for backwards compatibility reasons
-// TODO remove in next breaking release
-export 'package:utopia_utils/utopia_utils.dart' show LoggerReporter;
-
 class UtopiaArch {
   const UtopiaArch._();
 }
