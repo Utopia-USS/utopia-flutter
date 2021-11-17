0da04d9a77091bbffc0eaf9b50eba513806bd87c
Jakub Walusiak <jakubwalusiak@gmail.com>
2021-11-17 17:02:12 +0100

    fix(utopia_save_file): fix ios platform code

--- packages/save_file/ios/Classes/UtopiaSaveFilePlugin.m
+++ packages/save_file/ios/Classes/UtopiaSaveFilePlugin.m
@@ -1,6 +1,6 @@
 #import "UtopiaSaveFilePlugin.h"
-#if __has_include(<utopia_save_file/save_file-Swift.h>)
-#import <save_file/utopia_save_file-Swift.h>
+#if __has_include(<utopia_save_file/utopia_save_file-Swift.h>)
+#import <utopia_save_file/utopia_save_file-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
