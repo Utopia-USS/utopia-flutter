ee862212c396d8b1b787ae07c3755ca1cd6ab0a7
Jakub Walusiak <jakubwalusiak@gmail.com>
2022-03-17 17:42:40 +0100

    fix(utopia_arch): deprecate useScopedNavigator

--- packages/arch/lib/src/navigation/scoped_navigator_state.dart
+++ packages/arch/lib/src/navigation/scoped_navigator_state.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 
-
+@Deprecated("Use context.navigator or navigatorKey passing")
 class ScopedNavigatorState {
   final GlobalKey<NavigatorState> navigatorKey;
 
--- packages/arch/lib/src/navigation/use_scoped_navigator.dart
+++ packages/arch/lib/src/navigation/use_scoped_navigator.dart
@@ -2,4 +2,5 @@ import 'package:flutter/cupertino.dart';
 import 'package:utopia_arch/src/navigation/scoped_navigator_state.dart';
 import 'package:utopia_hooks/utopia_hooks.dart';
 
+@Deprecated("Use context.navigator or navigatorKey passing")
 NavigatorState useScopedNavigator() => useProvided<ScopedNavigatorState>().navigatorKey.currentState!;
