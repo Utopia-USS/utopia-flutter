938e9870795d1b1ffbc910ac1ae6ac71f7be51b3
Jakub Walusiak <jakubwalusiak@gmail.com>
2022-05-23 15:21:33 +0200

    fix(utopia_hooks): fix useStreamSubscription memoization issue

--- packages/hooks/lib/src/hook/stream/use_stream_subscription.dart
+++ packages/hooks/lib/src/hook/stream/use_stream_subscription.dart
@@ -4,7 +4,7 @@ import 'package:utopia_hooks/src/hook/misc/use_value_wrapper.dart';
 void useStreamSubscription<T>(Stream<T>? stream, void Function(T) block) {
   final wrappedBlock = useValueWrapper(block);
   useEffect(() {
-    final subscription = stream?.listen(wrappedBlock.value);
+    final subscription = stream?.listen((it) => wrappedBlock()(it));
     return () => subscription?.cancel();
   }, [stream]);
 }
