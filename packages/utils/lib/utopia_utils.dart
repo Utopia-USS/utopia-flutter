f940926a80920f9a23a6a1d565cec197b9efe88a
Jakub Walusiak <jakubwalusiak@gmail.com>
2022-07-29 14:23:15 +0200

    fix(utopia_utils): Include HasInitialized in utopia_utils.dart

--- packages/utils/lib/utopia_utils.dart
+++ packages/utils/lib/utopia_utils.dart
@@ -2,6 +2,7 @@ export 'src/comparator/compare_by.dart';
 export 'src/error/retryable.dart';
 export 'src/error/run_with_reporter_and_ui_errors.dart';
 export 'src/function/bool_functions.dart';
+export 'src/initialization/has_initialized.dart';
 export 'src/reporter/logger_reporter.dart';
 export 'src/reporter/reporter.dart';
 export 'src/reporter/run_app_with_reporter.dart';
