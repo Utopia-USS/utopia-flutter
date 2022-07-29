f4184f733afeb8569a0f534d46fbe5efcc616133
Jakub Walusiak <jakubwalusiak@gmail.com>
2022-07-29 14:24:08 +0200

    fix(utopia_lints): Remove invalid code from utopia_lints.dart

--- packages/lints/bin/custom_lint.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:isolate';
-
-import 'package:custom_lint_builder/custom_lint_builder.dart';
-import 'package:utopia_lints/utopia_lints.dart';
-
-void main(List<String> args, SendPort sendPort) {
-  startPlugin(sendPort, UtopiaLints());
-}
