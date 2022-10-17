1d0d6b8fd6b20ee26a08e0d393cfecce03f69768
Jakub Walusiak <jakubwalusiak@gmail.com>
2022-10-17 13:04:04 +0200

    feat(utopia_firebase_crashlytics): Expose UtopiaFirebaseCrashlytics.ensure() method

--- packages/firebase_crashlytics/lib/src/reporter/crashlytics_reporter.dart
+++ packages/firebase_crashlytics/lib/src/reporter/crashlytics_reporter.dart
@@ -1,8 +1,5 @@
-import 'dart:async';
-
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:utopia_firebase_crashlytics/utopia_firebase_crashlytics.dart';
 import 'package:utopia_utils/utopia_utils.dart';
 
 class CrashlyticsReporter extends Reporter {
@@ -10,37 +7,27 @@ class CrashlyticsReporter extends Reporter {
 
   @override
   void flutterError(FlutterErrorDetails details) =>
-      _runWithFirebaseRetry(() => FirebaseCrashlytics.instance.recordFlutterError(details));
+      UtopiaFirebaseCrashlytics.ensure((crashlytics) => crashlytics.recordFlutterError(details));
 
   @override
   void error(String message, {Object? e, StackTrace? s, String? sanitizedMessage}) {
-    _runWithFirebaseRetry(() {
+    UtopiaFirebaseCrashlytics.ensure((crashlytics) {
       final effectiveMessage = sanitizedMessage ?? message;
-      FirebaseCrashlytics.instance.recordError(e ?? effectiveMessage, s, reason: effectiveMessage);
+      crashlytics.recordError(e ?? effectiveMessage, s, reason: effectiveMessage);
     });
   }
 
   @override
-  void warning(String message, {Object? e, StackTrace? s, String? sanitizedMessage}) => _log(message, sanitizedMessage);
+  void warning(String message, {Object? e, StackTrace? s, String? sanitizedMessage}) =>
+      _log(message, e, sanitizedMessage);
 
   @override
-  void info(String message, {Object? e, StackTrace? s, String? sanitizedMessage}) => _log(message, sanitizedMessage);
+  void info(String message, {Object? e, StackTrace? s, String? sanitizedMessage}) => _log(message, e, sanitizedMessage);
 
-  void _log(String message, String? sanitizedMessage) {
-    _runWithFirebaseRetry(() {
-      final effectiveMessage = sanitizedMessage ?? message;
-      FirebaseCrashlytics.instance.log(effectiveMessage);
+  void _log(String message, Object? e, String? sanitizedMessage) {
+    UtopiaFirebaseCrashlytics.ensure((crashlytics) {
+      final effectiveMessage = (sanitizedMessage ?? message) + (e != null ? " --- $e" : "");
+      crashlytics.log(effectiveMessage);
     });
   }
-
-  void _runWithFirebaseRetry(void Function() block) {
-    if (!_isFirebaseInitialized) {
-      // we want to be sure that all errors are reported so we'll retry until firebase is initialized
-      unawaited(Future.delayed(const Duration(seconds: 1), () => _runWithFirebaseRetry(block)));
-    } else {
-      block();
-    }
-  }
-
-  bool get _isFirebaseInitialized => Firebase.apps.isNotEmpty;
 }
--- packages/firebase_crashlytics/lib/utopia_firebase_crashlytics.dart
+++ packages/firebase_crashlytics/lib/utopia_firebase_crashlytics.dart
@@ -1,14 +1,25 @@
 import 'dart:async';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 
 export 'src/reporter/crashlytics_reporter.dart';
 
 class UtopiaFirebaseCrashlytics {
+  static bool get _isFirebaseInitialized => Firebase.apps.isNotEmpty;
+
   static Future<void> setup() async {
     if (kDebugMode) {
       await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(false);
+    }
+  }
+
+  static void ensure(void Function(FirebaseCrashlytics crashlytics) block) {
+    if (!_isFirebaseInitialized) {
+      unawaited(Future.delayed(const Duration(seconds: 1), () => ensure(block)));
+    } else {
+      block(FirebaseCrashlytics.instance);
     }
   }
 }
