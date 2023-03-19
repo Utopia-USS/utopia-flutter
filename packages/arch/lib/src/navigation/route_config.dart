7ea80ea6dcb80089e28deb9d46006fbcf02a2828
jakub.walusiak <jakub.walusiak@proexe.pl>
2023-03-19 17:50:04 +0100

    fix(utopia_arch): Change deprecated imports

--- packages/arch/lib/src/navigation/route_config.dart
+++ packages/arch/lib/src/navigation/route_config.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:utopia_utils/utopia_utils_extensions.dart';
+import 'package:utopia_utils/utopia_utils.dart';
 
 enum RouteConfigOrientation {
   all,
