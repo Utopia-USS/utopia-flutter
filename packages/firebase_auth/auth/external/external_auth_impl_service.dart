75641b22f5b0327d88c81eb82186e98e59ed80d6
jakub.walusiak <jakub.walusiak@proexe.pl>
2023-06-06 17:26:34 +0200

    chore: Remove unneded files

--- packages/firebase_auth/auth/external/external_auth_impl_service.dart
+++
@@ -1,9 +0,0 @@
-import 'package:firebase_auth/firebase_auth.dart';
-
-abstract class ExternalAuthImplService {
-  bool get isSupported => true;
-
-  Future<AuthCredential?> authenticate();
-
-  Future<void> deAuthenticate();
-}
