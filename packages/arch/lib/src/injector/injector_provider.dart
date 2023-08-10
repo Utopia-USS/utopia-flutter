e6e9fb5891c151847c7ef619053370a8720ca7a4
jakub.walusiak <jakub.walusiak@proexe.pl>
2023-08-10 16:43:48 +0200

    feat(utopia_arch): Automatically rebuild injector on reassemble

--- packages/arch/lib/src/injector/injector_provider.dart
+++ packages/arch/lib/src/injector/injector_provider.dart
@@ -1,20 +1,31 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:injector/injector.dart';
-import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 import 'package:utopia_hooks/utopia_hooks.dart';
 
+T useInjected<T>() => useProvided<Injector>().get<T>();
+
 class InjectorProvider extends SingleChildStatelessWidget {
   final Injector Function() setupInjector;
+  final bool resetOnReassemble;
 
-  const InjectorProvider({super.key, required this.setupInjector});
+  const InjectorProvider({super.key, super.child, required this.setupInjector, this.resetOnReassemble = true});
 
   @override
   Widget buildWithChild(BuildContext context, Widget? child) {
-    return HookBuilder(
-      builder: (context) => Provider<Injector>.value(value: useMemoized(setupInjector), child: child),
-    );
+    return HookStateProvider(child: child, () {
+      final initial = useMemoized(setupInjector);
+      final current = useRef(initial);
+
+      if(kDebugMode) {
+        // Rebuild is guaranteed to happen after reassemble, so useRef is safe.
+        useReassemble(() {
+          if(resetOnReassemble) current.value = setupInjector();
+        });
+      }
+
+      return current.value;
+    });
   }
 }
-
-T useInjected<T>() => useSingleProvided<Injector>().get<T>();
