7af7401a9e3028ff7484c3d764d5fdc1e48dd34b
jakub.walusiak <jakub.walusiak@proexe.pl>
2023-08-11 13:34:02 +0200

    fix(utopia_arch): Remove nullability on routeArgs

--- packages/arch/lib/src/extension/context_extensions.dart
+++ packages/arch/lib/src/extension/context_extensions.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 
 extension ContextExtensions on BuildContext {
   NavigatorState get navigator => Navigator.of(this);
-  T? routeArgs<T>() => ModalRoute.of(this)!.settings.arguments as T;
+  T routeArgs<T>() => ModalRoute.of(this)!.settings.arguments as T;
 }
