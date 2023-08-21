00ba3a0363ab081e31431a760868f88eb90c2507
Jakub Walusiak <jakubwalusiak@gmail.com>
2023-08-21 21:43:04 +0200

    feat(utopia_rest): Add AuthTokenInterceptor.enabled

--- packages/rest/lib/src/interceptor/auth/auth_token_interceptor.dart
+++ packages/rest/lib/src/interceptor/auth/auth_token_interceptor.dart
@@ -3,6 +3,8 @@ import 'dart:async';
 import 'package:dio/dio.dart';
 
 class AuthTokenInterceptor extends Interceptor {
+  static const enabled = "AuthTokenInterceptor.enabled";
+
   final FutureOr<String?> Function() tokenProvider;
   final String headerName;
   final String Function(String token)? headerValueBuilder;
@@ -15,8 +17,9 @@ class AuthTokenInterceptor extends Interceptor {
 
   @override
   Future<void> onRequest(RequestOptions options, RequestInterceptorHandler handler) async {
+    if(options.extra[enabled] == false) return handler.next(options);
     final token = await tokenProvider();
-    if(token != null) {
+    if (token != null) {
       options.headers[headerName] = headerValueBuilder?.call(token) ?? 'Bearer $token';
     }
     handler.next(options);
