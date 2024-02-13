3ef21c041c2be4dc9ba7ccf97912f192893ea624
Jakub Walusiak <jakubwalusiak@gmail.com>
2024-02-13 15:22:07 +0100

    fix(utopia_hooks): Check for isMounted in usePeriodicalSignal

--- packages/hooks/lib/src/hook/async/use_periodical_signal.dart
+++ packages/hooks/lib/src/hook/async/use_periodical_signal.dart
@@ -1,15 +1,19 @@
 import 'dart:async';
 
 import 'package:utopia_hooks/src/hook/base/use_effect.dart';
+import 'package:utopia_hooks/src/hook/base/use_is_mounted.dart';
 
 import '../base/use_state.dart';
 
 Object usePeriodicalSignal({required Duration period, bool enabled = true}) {
   final state = useState(0);
+  final isMounted = useIsMounted();
 
   useEffect(() {
     if (enabled) {
-      final timer = Timer.periodic(period, (_) => state.value++);
+      final timer = Timer.periodic(period, (_) {
+        if(isMounted()) state.value++;
+      });
       return timer.cancel;
     }
     return null;
