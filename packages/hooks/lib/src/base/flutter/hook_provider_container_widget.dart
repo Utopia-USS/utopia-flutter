cc289715a608612508168f4c7a02d7c0227bc87c
Jakub Walusiak <jakubwalusiak@gmail.com>
2024-05-10 14:51:35 +0200

    fix(utopia_hooks): Expose HookProviderContainerWidgetMixin and HookProviderContainerWidgetStateMixin

--- packages/hooks/lib/src/base/flutter/hook_provider_container_widget.dart
+++ packages/hooks/lib/src/base/flutter/hook_provider_container_widget.dart
@@ -6,59 +6,71 @@ import 'package:flutter/scheduler.dart';
 import 'package:utopia_hooks/src/base/provider/hook_provider_container.dart';
 import 'package:utopia_hooks/src/provider/provider_widget.dart';
 
-class HookProviderContainerWidget extends StatefulWidget {
+mixin HookProviderContainerWidgetMixin on StatefulWidget {
+  abstract final Map<Type, Object? Function()> providers;
+  abstract final Priority schedulerPriority;
+  abstract final Widget child;
+}
+
+class HookProviderContainerWidget extends StatefulWidget with HookProviderContainerWidgetMixin {
+  @override
   final Map<Type, Object? Function()> providers;
-  final Widget child;
+  @override
   final Priority schedulerPriority;
+  @override
+  final Widget child;
 
   const HookProviderContainerWidget(
     this.providers, {
-    required this.child,
-    this.schedulerPriority = Priority.animation,
     super.key,
+    this.schedulerPriority = Priority.animation,
+    required this.child,
   });
 
   @override
   State<HookProviderContainerWidget> createState() => _HookProviderContainerWidgetState();
 }
 
-class _HookProviderContainerWidgetState extends State<HookProviderContainerWidget> with DiagnosticableTreeMixin {
-  late final HookProviderContainer _container;
+class _HookProviderContainerWidgetState extends State<HookProviderContainerWidget>
+    with DiagnosticableTreeMixin, HookProviderContainerWidgetStateMixin {}
+
+mixin HookProviderContainerWidgetStateMixin<W extends HookProviderContainerWidgetMixin>
+    on State<W>, DiagnosticableTreeMixin {
+  @protected
+  late final HookProviderContainer container;
   late Map<Type, Object?> _values;
   var _isFirstBuild = true;
 
-  @override
-  void initState() {
-    super.initState();
-    _container = HookProviderContainer(schedule: _schedule);
-    _container.initialize({BuildContext: () => context, ...widget.providers});
-    _values = {
-      for (final type in widget.providers.keys) type: _container.getUnsafe(type),
-    };
-    for (final entry in widget.providers.keys) {
-      _container.addListenerUnsafe(entry, (value) => setState(() => _values[entry] = value));
-    }
-  }
+  @protected
+  Map<Type, Object Function()> get additionalProviders => {};
 
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
     if (_isFirstBuild) {
+      container = HookProviderContainer(schedule: _schedule);
+      container.initialize({BuildContext: () => context, ...additionalProviders, ...widget.providers});
+      _values = {
+        for (final type in widget.providers.keys) type: container.getUnsafe(type),
+      };
+      for (final entry in widget.providers.keys) {
+        container.addListenerUnsafe(entry, (value) => setState(() => _values[entry] = value));
+      }
       _isFirstBuild = false;
     } else {
-      _container.refresh(_container.getDependents(BuildContext));
+      container.refresh(container.getDependents(BuildContext));
     }
   }
 
   @override
   void reassemble() {
     super.reassemble();
-    _container.reassemble();
+    container.reassemble();
   }
 
   @override
   void dispose() {
-    _container.dispose();
+    container.dispose();
     super.dispose();
   }
 
@@ -78,6 +90,6 @@ class _HookProviderContainerWidgetState extends State<HookProviderContainerWidge
     properties.add(
       FlagProperty("first build", value: _isFirstBuild, ifTrue: "first build", level: DiagnosticLevel.debug),
     );
-    properties.add(DiagnosticableTreeNode(name: "container", value: _container, style: null));
+    properties.add(DiagnosticableTreeNode(name: "container", value: container, style: null));
   }
 }
