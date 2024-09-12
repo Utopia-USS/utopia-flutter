5f753a2840ad339a5c501cfb7f3a86546d7ca536
Jakub Walusiak <jakubwalusiak@gmail.com>
2024-09-12 10:22:04 +0200

    feat(utopia_hooks): Add useLet

--- packages/hooks/lib/src/hook/nested/use_keyed.dart
+++ packages/hooks/lib/src/hook/nested/use_keyed.dart
@@ -4,10 +4,14 @@ import 'package:utopia_hooks/src/base/hook_context.dart';
 import 'package:utopia_hooks/src/base/hook_keys.dart';
 import 'package:utopia_hooks/src/base/nested_hook.dart';
 import 'package:utopia_hooks/src/hook/nested/use_debug_group.dart';
+import 'package:utopia_utils/utopia_utils.dart';
 
 // ignore: avoid_positional_boolean_parameters
 T? useIf<T>(bool condition, T Function() block) =>
     useDebugGroup(debugLabel: 'useIf<$T>()', () => useKeyed([condition], () => condition ? block() : null));
+
+R? useLet<T extends Object, R>(T? value, R Function(T) block) =>
+    useDebugGroup(debugLabel: 'useLet<$T, $R>()', () => useKeyed([value], () => value?.let(block)));
 
 T useKeyed<T>(HookKeys keys, T Function() block) => use(_KeyedHook(keys, block));
 
