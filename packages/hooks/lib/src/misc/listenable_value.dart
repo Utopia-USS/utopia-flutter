e460e9f3417ff10804974519eefa38082e73f8ff
jakub <jakub@utopiasoft.io>
2025-02-27 14:03:45 +0100

    feat(utopia_hooks): Improvements to ListenableValue

--- packages/hooks/lib/src/misc/listenable_value.dart
+++ packages/hooks/lib/src/misc/listenable_value.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/foundation.dart';
 import 'package:utopia_utils/utopia_utils.dart';
 
-abstract class ListenableValue<T> implements Value<T>, Listenable {}
+abstract class ListenableValue<T> implements Value<T>, Listenable {
+  const factory ListenableValue(T value) = ListenableValueImpl;
+}
 
 abstract class ListenableMutableValue<T> implements MutableValue<T>, ListenableValue<T> {
   factory ListenableMutableValue(T value) = ListenableMutableValueImpl;
 }
 
+class ListenableValueImpl<T> extends ValueImpl<T> with NeverTriggeringListenable implements ListenableValue<T> {
+  const ListenableValueImpl(super.value);
+}
+
 class ListenableMutableValueImpl<T> extends MutableValueImpl<T>
     with ChangeNotifier
     implements ListenableMutableValue<T> {
-  ListenableMutableValueImpl(super.value);
+  final void Function()? onDisposed;
+
+  ListenableMutableValueImpl(super.value, {this.onDisposed});
 
   @override
   set value(T value) {
@@ -18,4 +26,21 @@ class ListenableMutableValueImpl<T> extends MutableValueImpl<T>
     super.value = value;
     notifyListeners();
   }
+
+  @override
+  void dispose() {
+    onDisposed?.call();
+    super.dispose();
+  }
+}
+
+/// Implementation of a [Listenable] that never triggers any listeners,
+///
+/// Since the listeners will never be called, they are not stored.
+mixin NeverTriggeringListenable implements Listenable {
+  @override
+  void addListener(VoidCallback listener) {}
+
+  @override
+  void removeListener(VoidCallback listener) {}
 }
